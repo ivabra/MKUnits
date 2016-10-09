4917184100b0b685927bd3e97bc3a4ec6c002d92
Ivan Brazhnikov <ivanbrazhnikov@ivabra-mac.Dlink>
2016-10-10 01:49:19 +0300

    updated project settings

---
+++ MKUnits/MKUnits/MKUnits.h
@@ -0,0 +1,19 @@
+//
+//  MKUnits.h
+//  MKUnits
+//
+//  Created by Ivan Brazhnikov on 30.09.16.
+//  Copyright © 2016 Ivan Brahnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKUnits.
+FOUNDATION_EXPORT double MKUnitsVersionNumber;
+
+//! Project version string for MKUnits.
+FOUNDATION_EXPORT const unsigned char MKUnitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKUnits/PublicHeader.h>
+
+
